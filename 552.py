f3f0580cebeae10fd49fcc4bc8c7f0695732cfe2
kamaligeetha <53109104+kamaligeetha@users.noreply.github.com>
2019-08-13 15:10:55 +0530

    Create 552.py

---
+++ 552.py
@@ -0,0 +1,22 @@
+P=int(input())
+if P==1:
+	print("One")
+elif P==2:
+	print("Two")
+elif P==3:
+	print("Three")
+elif P==4:
+	print("Four")
+elif P==5:
+	print("Five")
+elif P==6:
+	print("Six")
+elif P==7:
+	print("Seven")
+elif P==8:
+	print("Eight")
+elif P==9:
+	print("Nine")
+else:
+	print("Ten")
+  #j
